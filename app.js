e4c63399047c422e4c8284f359832e7ac00f41e0
ValeriaSaterdinova <vsaterdinova.exceedteam@gmail.com>
2021-11-12 14:28:51 +0300

    fix error

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const taskScheme = new Schema({
 
 const Task = mongoose.model("tasks", taskScheme);
 
-const uri = 'mongodb+srv://ValeriaSaterdinova:restart987*@education.cssf9.mongodb.net/To-doList?retryWrites=true&w=majority'
+const uri = 'mongodb+srv://ValeriaSaterdinova:restart987*@education.cssf9.mongodb.net/To-doList?retryWrites=true&w=majority';
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.use(bodyParser.json());
@@ -24,26 +24,39 @@ app.get('/allTasks', (req, res) => {
 });
 
 app.post('/createTask', (req, res) => {
-  const task = new Task(req.body)
-  task.save().then(result => {
-    res.send('Task created');
-  })
+  if(req.body.hasOwnProperty('text') && req.body.hasOwnProperty('isCheck')){
+    const task = new Task(req.body);
+    task.save().then(result => {
+      res.send('Task created');
+    });
+  } else {
+    res.status(422).send('Incorrect parameters');
+  };
 });
 
 app.patch('/updateTask', (req, res) => {
-  Task.updateOne(req.body._id, req.body).then(result => {
-    Task.find(req.body._id).then(result => {
+  if(req.body.hasOwnProperty('_id') && (req.body.hasOwnProperty('text') || req.body.hasOwnProperty('isCheck'))) {
+  Task.updateOne({_id: req.body._id}, req.body).then(result => {
+    Task.find({_id: req.body._id}).then(result => {
       res.send({data: result});
     });
-  }); 
+  });
+  } else {
+    res.status(422).send('Incorrect parameters');
+  };
 });
 
 app.delete('/deleteTask', (req, res) => {
-  Task.deleteOne(req.query._id).then(result => {
-    res.send('Task deleted');
-  })
-});
+  if(req.query.hasOwnProperty('_id')) {
+    Task.deleteOne({_id: req.query._id}).then(result => {
+      res.send('Task deleted');
+    });
+  } else {
+    res.status(422).send('Incorrect parameters');
+  };
+  });
+
 
 app.listen(8000, () => {
-  console.log('Example app listening on port 8000!')
+  console.log('Example app listening on port 8000!');
 });
